4c2fc7a2a0bfb85be3e96286679c020266e64120
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-12-30 19:11:25 -0600

    Update transferOwner.sol

--- ownership/solutions/transferOwner.sol
+++ ownership/solutions/transferOwner.sol
@@ -1,4 +1,4 @@
-function transferOwnership(address newOwner) external onlyOwner {
+function safeTransferOwnership(address newOwner) external onlyOwner {
   require(newOwner!=address(0), "Owned: Address must not be null");
   require(candidate Owner != newOwner, "Owned: Same candidate owner"); 
   candidate Owner = newOwner;
@@ -8,5 +8,5 @@ function transferOwnership(address newOwner) external onlyOwner {
 function claimOwner() external {
   require(candidate Owner == msg.sender, "Owned: Claim ownership failed"); 
   owner = candidate Owner;
-  emit OwnerChanged (candidate Owner);
+  emit OwnerChanged(candidate Owner);
   }
