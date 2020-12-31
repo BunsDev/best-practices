f59d50efae529d98b168347f651a94093c9b2782
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-12-30 19:13:02 -0600

    Update transferOwner.sol

--- ownership/solutions/transferOwner.sol
+++ ownership/solutions/transferOwner.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.7.0;
 
-function safeTransferOwnership(address newOwner) external onlyOwner {
+function safeTransferOwnership(address newOwner) external onlyOwner 
+{
   require(newOwner!=address(0), "Owned: Address must not be null");
   require(candidate Owner != newOwner, "Owned: Same candidate owner"); 
   candidate Owner = newOwner;
-  }
+ }
 
 // new owner claims ownership
-function claimOwner() external {
+function claimOwner() external 
+{
   require(candidate Owner == msg.sender, "Owned: Claim ownership failed"); 
   owner = candidate Owner;
   emit OwnerChanged(candidate Owner);
-  }
+ }
